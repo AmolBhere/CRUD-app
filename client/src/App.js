cc95e5fd517024dd803883b7fa53d5ef84c45fe7
Amol Bhere <amolbhere2003@gmail.com>
2023-12-12 13:27:28 +0530

    update

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Books from "./Pages/Books";
+import Add from "./Pages/Add";
+import Update from "./Pages/Update";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/update" element={<Update/>}/>
+        </Routes>
+      </BrowserRouter>
+      Hello
+    </div>
+  );
+}
+
+export default App;
